aed6264e3df0e0b3cee4a7984f79ad42c3a983cc
yone <yone@yone-himajin.com>
2022-11-06 16:52:08 +0900

    builder fix

--- design_pattern/Builder/Main.java
+++ design_pattern/Builder/Main.java
@@ -2,27 +2,27 @@ package design_pattern.Builder;
 
 public class Main {
     public static void main(String[] args){
-        // if(args.length != 1){
-        //     usage();
-        //     System.exit(0);
-        // }
+        if(args.length != 1){
+            usage();
+            System.exit(0);
+        }
 
-        // if(args[0].equals("plain")){
-        //     TextBuilder textbuilder = new TextBuilder();
-        //     Director director = new Director(textbuilder);
-        //     director.construct();
-        //     String result = textbuilder.getResult();
-        //     System.out.println(result);
-        // // }else if(args[0].equals("html")){
+        if(args[0].equals("plain")){
+            TextBuilder textbuilder = new TextBuilder();
+            Director director = new Director(textbuilder);
+            director.construct();
+            String result = textbuilder.getResult();
+            System.out.println(result);
+        }else if(args[0].equals("html")){
             HtmlBuilder htmlBuilder = new HtmlBuilder();
             Director director = new Director(htmlBuilder);
             director.construct();
             String fileName = htmlBuilder.getResult();
             System.out.println(fileName + "が作成されました。");
-        // }else{
-        //     usage();
-        //     System.exit(0);
-        // }
+        }else{
+            usage();
+            System.exit(0);
+        }
 
     }
 
